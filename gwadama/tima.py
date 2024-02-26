8750ef08d0551f65b1996f2dc31931fcafcc2107
Miquel Llorens <miquel.llorens@uv.es>
2024-02-26 11:11:27 +0100

    make more general the resampling function

--- gwadama/tima.py
+++ gwadama/tima.py
@@ -12,7 +12,10 @@ from scipy.interpolate import make_interp_spline as sp_make_interp_spline
 
 
 
-def resample(strain: np.ndarray, sample_rate: int) -> tuple[np.ndarray, int, int]:
+def resample(strain: np.ndarray,
+             time: np.ndarray | int,
+             sample_rate: int,
+             full_output=True) -> tuple[np.ndarray, int, int]:
     """Resample a single strain in time domain.
     
     Resample strain's sampling rate using an interpolation in the time domain
@@ -25,42 +28,50 @@ def resample(strain: np.ndarray, sample_rate: int) -> tuple[np.ndarray, int, int
 
     PARAMETERS
     ----------
-    strain: 2d-array
-        NDArray containing the time samples and both polarizations, with shape
-        (time, hplus, hcross).
+    strain: 1d-array
+        Only one strain.
+    
+    time: 1d-array | int
+        Time points. If an Int is given, it is interpreted as the former
+        sampling rate, and assumed to be constant.
     
     sample_rate: int
         Target sample rate.
     
+    full_output: bool, optional
+        If True, also returns the upscaled sampling rate and the factor down.
+    
         
     RETURNS
     -------
-    strain_resampled: 2d-array
-        NDArray with the same shape as the input strain: (time, hplus, hcross).
+    strain: 1d-array
+        Strain at the new sampling rate.
     
-    sr_up: int
+    sr_up: int, optional
         Upscaled sample rate.
     
-    factor_down: int
+    factor_down: int, optional
         Factor at which the signal is decimated after the upscalling.
     
     """
-    time, hplus, hcros = strain
+    if isinstance(time, np.ndarray):
+        sr_max = 1 / np.min(np.diff(time))
+    elif isinstance(time, int):
+        sr_max = time
+    else:
+        raise TypeError("'time' type not recognized")
 
     # Upsample:
-    sr_max = 1 / np.min(np.diff(time))
+    #
     sr_up = int((sr_max // sample_rate + 1) * sample_rate)
     # Intentionally skipping last time point to avoid extrapolation by round-off errors.
-    time_up = np.arange(time[0], time[-1], 1/sr_up)
-    hplus_up = sp_make_interp_spline(time, hplus, k=2)(time_up)
-    hcros_up = sp_make_interp_spline(time, hcros, k=2)(time_up)
+    time = np.arange(time[0], time[-1], 1/sr_up)
+    strain = sp_make_interp_spline(time, strain, k=2)(time)
 
     # Downsample:
+    #
     factor_down = sr_up // sample_rate
-    time_re = time_up[::factor_down]
-    hplus_re = sp.signal.decimate(hplus_up, factor_down, ftype='fir')
-    hcros_re = sp.signal.decimate(hcros_up, factor_down, ftype='fir')
+    time = time[::factor_down]
+    strain = sp.signal.decimate(strain, factor_down, ftype='fir')
     
-    strain_resampled = np.asarray([time_re, hplus_re, hcros_re])
-    
-    return strain_resampled, sr_up, factor_down
+    return strain, sr_up, factor_down if full_output else strain
