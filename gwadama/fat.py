3496e9f9eb5d81103dc0a86f67dd594c8b75168b
Miquel Llorens <miquel.llorens@uv.es>
2024-05-10 16:39:58 +0200

    implement padding in the original whitening function

--- gwadama/fat.py
+++ gwadama/fat.py
@@ -10,7 +10,16 @@ from gwpy.timeseries import TimeSeries
 import numpy as np
 
 
-def whiten(strain, *, asd, margin, sample_rate, fduration, highpass, normed=True, **kwargs):
+def whiten(strain: np.ndarray,
+           *,
+           asd: np.ndarray,
+           sample_rate: int,
+           flength: int,
+           highpass: float = None,
+           pad: int = 0,
+           unpad: int = 0,
+           normed: bool = True,
+           **kwargs) -> np.ndarray:
     """Whiten a single strain signal.
 
     Whiten a strain using the input amplitude spectral density 'asd',
@@ -22,25 +31,34 @@ def whiten(strain, *, asd, margin, sample_rate, fduration, highpass, normed=True
     strain : NDArray
         Strain data points in time domain.
     
-    asd : FrequencySeries or 2d-array
+    asd : 2d-array
         Amplitude spectral density assumed for the 'set_strain'.
-        If a 2d-array is given, its components are:
+        Its components are:
         - asd[0] = frequency points
         - asd[1] = ASD points
         NOTE: It must has a linear and constant sampling rate!
     
-    margin : int
-        Marging at each side of the strain to crop in order to avoid vigneting
-        due to the windowing during the whitening.
-        The corrupted area at each side is `0.5*fduration` in GWpy's whiten(),
-        which by default is 2 s, thus 1 s at each side.
-    
     sample_rate : int
-
-    fduration : int
-        Length (in samples) of the time-domain FIR whitening filter.
+        The thingy that makes things do correctly their thing.
     
-    highpass : int
+    flength : int
+        Length (in samples) of the time-domain FIR whitening filter.
+        Passed in seconds (`flength/sample_rate`) to GWpy's whiten() function
+        as the 'fduration' parameter.
+    
+    pad : int, optional
+        Marging at each side of the strain to add (zero-pad) in order to avoid
+        vigneting. The corrupted area at each side is `0.5 * fduration` in
+        GWpy's whiten().
+        Will be cropped afterwards, thus no samples are added at the end of
+        the call to this function.
+        If given, 'unpad' will be ignored.
+    
+    unpad : int, optional
+        Marging at each side of the strain to crop.
+        Will be ignored if 'pad' is given.
+        
+    highpass : float, optional
         Highpass corner frequency (in Hz) of the FIR whitening filter.
     
     normed : bool
@@ -55,18 +73,27 @@ def whiten(strain, *, asd, margin, sample_rate, fduration, highpass, normed=True
         Whitened strain (in time domain).
 
     """
-    if not isinstance(asd, FrequencySeries):
-        asd = FrequencySeries(asd[1], frequencies=asd[0])
+    if asd.ndim != 2:
+        raise ValueError("'asd' must have 2 dimensions")
+
+    if not isinstance(flength, int):
+        raise TypeError("'flength' must be an integer")
+
+    _asd = FrequencySeries(asd[1], frequencies=asd[0])
+
+    if pad > 0:
+        strain = np.pad(strain, pad, 'constant', constant_values=0)
+        unpad = pad
  
     frame = TimeSeries(strain, sample_rate=sample_rate)
     strain_w = frame.whiten(
-        asd=asd,
-        fduration=fduration/sample_rate,  # to seconds
+        asd=_asd,
+        fduration=flength/sample_rate,  # to seconds
         highpass=highpass,
         **kwargs
-    ).value
-    strain_w = strain_w[margin:-margin]
-
+    ).value  # Convert to numpy array!!!
+    
+    strain_w = strain_w[unpad:-unpad]
     if normed:
         strain_w /= np.max(np.abs(strain_w))
 
