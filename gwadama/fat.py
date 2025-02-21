10a73309b91fd374fa291439e4b3cb3a6aebcad1
Miquel Llorens <miquel.llorens@uv.es>
2025-02-21 18:01:34 +0100

    comment out previous whiten version

--- gwadama/fat.py
+++ gwadama/fat.py
@@ -5,105 +5,103 @@ Frequency analysis toolkit.
 """
 import warnings
 
-from gwpy.frequencyseries import FrequencySeries
-from gwpy.timeseries import TimeSeries
 import numpy as np
 import scipy as sp
 
 
 
-def whiten(strain: np.ndarray,
-           *,
-           asd: np.ndarray,
-           sample_rate: int,
-           flength: int,
-           highpass: float = None,
-           pad: int = 0,
-           unpad: int = 0,
-           normed: bool = True,
-           **kwargs) -> np.ndarray:
-    """Whiten a single strain signal.
+# def whiten(strain: np.ndarray,
+#            *,
+#            asd: np.ndarray,
+#            sample_rate: int,
+#            flength: int,
+#            highpass: float = None,
+#            pad: int = 0,
+#            unpad: int = 0,
+#            normed: bool = True,
+#            **kwargs) -> np.ndarray:
+#     """Whiten a single strain signal.
 
-    Whiten a strain using the input amplitude spectral density 'asd',
-    and shrinking signals afterwarwds to 'l_window' to account for the edge
-    effects introduced by the windowing.
+#     Whiten a strain using the input amplitude spectral density 'asd',
+#     and shrinking signals afterwarwds to 'l_window' to account for the edge
+#     effects introduced by the windowing.
 
-    Parameters
-    ----------
-    strain : NDArray
-        Strain data points in time domain.
+#     Parameters
+#     ----------
+#     strain : NDArray
+#         Strain data points in time domain.
     
-    asd : 2d-array
-        Amplitude spectral density assumed for the 'set_strain'.
-        Its components are:
-        - asd[0] = frequency points
-        - asd[1] = ASD points
-        NOTE: It must has a linear and constant sampling frequency!
+#     asd : 2d-array
+#         Amplitude spectral density assumed for the 'set_strain'.
+#         Its components are:
+#         - asd[0] = frequency points
+#         - asd[1] = ASD points
+#         NOTE: It must has a linear and constant sampling frequency!
     
-    sample_rate : int
-        The thingy that makes things do correctly their thing.
+#     sample_rate : int
+#         The thingy that makes things do correctly their thing.
     
-    flength : int
-        Length (in samples) of the time-domain FIR whitening filter.
-        Passed in seconds (`flength/sample_rate`) to GWpy's whiten() function
-        as the 'fduration' parameter.
+#     flength : int
+#         Length (in samples) of the time-domain FIR whitening filter.
+#         Passed in seconds (`flength/sample_rate`) to GWpy's whiten() function
+#         as the 'fduration' parameter.
     
-    pad : int, optional
-        Marging at each side of the strain to add (zero-pad) in order to avoid
-        edge effects. The corrupted area at each side is `0.5 * fduration` in
-        GWpy's whiten().
-        Will be cropped afterwards, thus no samples are added at the end of
-        the call to this function.
-        If given, 'unpad' will be ignored.
+#     pad : int, optional
+#         Marging at each side of the strain to add (zero-pad) in order to avoid
+#         edge effects. The corrupted area at each side is `0.5 * fduration` in
+#         GWpy's whiten().
+#         Will be cropped afterwards, thus no samples are added at the end of
+#         the call to this function.
+#         If given, 'unpad' will be ignored.
     
-    unpad : int, optional
-        Marging at each side of the strain to crop.
-        Will be ignored if 'pad' is given.
+#     unpad : int, optional
+#         Marging at each side of the strain to crop.
+#         Will be ignored if 'pad' is given.
         
-    highpass : float, optional
-        Highpass corner frequency (in Hz) of the FIR whitening filter.
+#     highpass : float, optional
+#         Highpass corner frequency (in Hz) of the FIR whitening filter.
     
-    normed : bool
-        If True, normalizes the strains to their maximum absolute amplitude.
+#     normed : bool
+#         If True, normalizes the strains to their maximum absolute amplitude.
 
-    **kwargs:
-        Extra arguments passed to gwpy.timeseries.Timeseries.whiten().
+#     **kwargs:
+#         Extra arguments passed to gwpy.timeseries.Timeseries.whiten().
     
-    Returns
-    -------
-    strain_w : NDArray
-        Whitened strain (in time domain).
+#     Returns
+#     -------
+#     strain_w : NDArray
+#         Whitened strain (in time domain).
 
-    """
-    if asd.ndim != 2:
-        raise ValueError("'asd' must have 2 dimensions")
+#     """
+#     if asd.ndim != 2:
+#         raise ValueError("'asd' must have 2 dimensions")
 
-    if not isinstance(flength, int):
-        raise TypeError("'flength' must be an integer")
+#     if not isinstance(flength, int):
+#         raise TypeError("'flength' must be an integer")
 
-    _asd = FrequencySeries(asd[1], frequencies=asd[0])
+#     _asd = FrequencySeries(asd[1], frequencies=asd[0])
 
-    if pad > 0:
-        strain = np.pad(strain, pad, 'constant', constant_values=0)
-        _unpad = slice(pad, -pad)
-    elif unpad == 0:
-        _unpad = slice(None)
-    else:
-        _unpad = slice(unpad, -unpad)
+#     if pad > 0:
+#         strain = np.pad(strain, pad, 'constant', constant_values=0)
+#         _unpad = slice(pad, -pad)
+#     elif unpad == 0:
+#         _unpad = slice(None)
+#     else:
+#         _unpad = slice(unpad, -unpad)
  
-    frame = TimeSeries(strain, sample_rate=sample_rate)
-    strain_w = frame.whiten(
-        asd=_asd,
-        fduration=flength/sample_rate,  # to seconds
-        highpass=highpass,
-        **kwargs
-    ).value  # Convert to numpy array!!!
+#     frame = TimeSeries(strain, sample_rate=sample_rate)
+#     strain_w = frame.whiten(
+#         asd=_asd,
+#         fduration=flength/sample_rate,  # to seconds
+#         highpass=highpass,
+#         **kwargs
+#     ).value  # Convert to numpy array!!!
     
-    strain_w = strain_w[_unpad]
-    if normed:
-        strain_w /= np.max(np.abs(strain_w))
+#     strain_w = strain_w[_unpad]
+#     if normed:
+#         strain_w /= np.max(np.abs(strain_w))
 
-    return strain_w
+#     return strain_w
 
 
 
